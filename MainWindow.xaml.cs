8b249b89e4b0f646b49e417d0b69cfb90de256f9
Fa4stik <stepan.sharifulin@mail.ru>
2022-10-20 01:01:47 +0500

    add interface from reestr

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PIS8_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public class ReestrInfo
+    {
+        public bool Check { get; set; } // выделение
+        public int NumMK { get; set; } // номер МК
+        public DateTime DataMK { get; set; } // дата МК
+        public string Munic { get; set; } // муниципальное образование
+        public string OMSU { get; set; } // ОМСУ
+        public string ExecMK { get; set; } // исполнитель МК
+        public int NumOrder { get; set; } // номер заказа
+        public string Locality { get; set; } // населённый пункт
+        public DateTime DataOrder { get; set; } // дата заказа
+        public DateTime DataCapture { get; set; } // дата отлова
+        public string PurposeCapture { get; set; } // цель отлова
+        public string CaptureOrder { get; set; } // заявка на отлов или план-график
+
+    }
+    public partial class MainWindow : Window
+    {
+        private BindingList<ReestrInfo> _reestrInfo;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dgReestr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dgReestr_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _reestrInfo = new BindingList<ReestrInfo>()
+            {
+                new ReestrInfo{ Check = true, NumMK = 12, DataMK = DateTime.Now, Munic = "Организация", OMSU = "ОМСУ", ExecMK = "Исполнитель", NumOrder=2341, Locality="Тюмень", DataOrder=DateTime.Now, DataCapture=DateTime.Now, PurposeCapture = "Цель отлова", CaptureOrder="План-график"}
+            };
+            dgReestr.ItemsSource = _reestrInfo;
+        }
+    }
+}
