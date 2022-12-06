d594c7c3e1528f4b735b0c946b161d9b3b018677
Fa4stik <stepan.sharifulin@mail.ru>
2022-12-06 22:00:58 +0500

    Edit class Connection
    Realise method ExecuteUser

--- MVVM/Model/Data/Connection.cs
+++ MVVM/Model/Data/Connection.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using System.Security.Policy;
 
 namespace PIS8_2.MVVM.Model.Data
 {
-    internal class Connection
+    public class Connection
     {
         //достаем из бд юзера или return null 
         public TUser ExecuteUser(string login, string password)
         {
-            return new TUser("12312");
+            var hashPassword = HashPassword(password);
+            using (var db = new trappinganimalsContext())
+            {
+                var user = db.Tusers.ToList();
+                if (user.Exists(s => s.Login == login && s.Passwordhash == hashPassword))
+                    return user.Where(s => s.Login == login).First();
+                return null;
+            }
+        }
+
+        static string HashPassword(string password)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToHexString(bytes).ToLower();
         }
     }
 }
