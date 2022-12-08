4fc75cc6c399994ac4a2ea7e3cf6e2e6e234616e
Fa4stik <stepan.sharifulin@mail.ru>
2022-12-08 22:25:03 +0500

    Auto stash before merge of "dev" and "origin2/dev"
    Add

--- MVVM/Model/Data/trappinganimalsContext.cs
+++ MVVM/Model/Data/trappinganimalsContext.cs
@@ -30,8 +30,7 @@ public partial class TrappinganimalsContext : DbContext
     public virtual DbSet<Tuser> Tusers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=trappinganimals;Username=postgres;Password=2476");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=trappinganimals;Username=postgres;Password=123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
