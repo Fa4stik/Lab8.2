4ee1f0be3a5735314125551e579fa9130361bb34
Дмитрий Базитов <stud0000229935@study.utmn.ru>
2022-12-08 14:21:13 +0500

    1

--- MVVM/Model/Data/Connection.cs
+++ MVVM/Model/Data/Connection.cs
@@ -14,10 +14,10 @@ namespace PIS8_2.MVVM.Model.Data
         //достаем из бд юзера или return null 
         public Tuser ExecuteUser(string login, string password)
         {
-            var hashPassword = HashPassword(password);
+            var hashPassword = (password);
             using (var db = new TrappinganimalsContext())
             {
-                var user = db.Tusers.Include(c => c.IdOrgNavigation).ToList();
+                var user = db.Tusers.Include(c => c.IdOrgNavigation).Include(c=>c.IdOmsuNavigation.IdMunicipNavigation).ToList();
                 return user.FirstOrDefault(u => u.Login == login && u.Passwordhash == hashPassword);
             }
         }
@@ -34,7 +34,7 @@ namespace PIS8_2.MVVM.Model.Data
             //var id = user.IdOrg ?? GetMunicip(user.IdOmsu);
             using (var db = new TrappinganimalsContext())
             {
-                return db.Cards.Include(c => c.IdOrgNavigation).Where(c => c.IdOrg == user.IdOrg).ToList();
+                return db.Cards.Include(c => c.IdOrgNavigation).Include(c => c.IdMunicipNavigation).Where(c => c.IdOrg == user.IdOrg).ToList();
             }
 
         }
