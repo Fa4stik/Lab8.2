1ea4c2377085ffaf5d687f4fc06d4df121b07c10
Fa4stik <stepan.sharifulin@mail.ru>
2022-12-23 16:30:01 +0500

    Edit model

---
+++ MVVM/Model/APIMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS8_2.MVVM.Model
+{
+    abstract class APIMethod
+    {
+
+    }
+}
